7044a26db603c545aed24657df950fb79adddcb2
Aarya_P <aarya.patil@spit.ac.in>
2023-10-24 15:14:09 +0530

    space

--- client/src/components/Button/index.js
+++ client/src/components/Button/index.js
@@ -4,7 +4,7 @@ const Button = ({
     label = 'Button',
     type = 'button',
     className = '',
-    disabled = false,
+    disabled = false, 
 }) => {
   return (
     <button type={type} className={`text-white bg-red-700 hover:bg-primary focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ${className}`} disabled={disabled}>{label}</button>
