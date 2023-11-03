633453d68b425b3f2a1ddded819a37b21c18a229
Aarya_P <aarya.patil@spit.ac.in>
2023-11-04 00:06:04 +0530

    adjusted map zoom

--- client/src/components/Mappu.jsx
+++ client/src/components/Mappu.jsx
@@ -21,7 +21,7 @@ const Mappu = () => {
     const markerPosition7=[18.986,72.8259];
     return ( 
       // Make sure you set the height and width of the map container otherwise the map won't show
-        <MapContainer center={[19.0190, 72.8426]} zoom={13} ref={mapRef} style={{height: "100vh", width: "100vw"}}>
+        <MapContainer center={[18.986,72.8259]} zoom={12} ref={mapRef} style={{height: "100vh", width: "100vw"}}>
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
